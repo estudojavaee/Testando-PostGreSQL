44d916c2c59b2aa6d57dc7b4bba338e31b0b06a9
Marcos <Marcos@Marcos-PC>
2015-07-23 21:13:11 -0300

    parte I acabada

---
+++ src/main/java/jsfjpa/managed/AutomovelBean.java
@@ -0,0 +1,60 @@
+package jsfjpa.managed;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transaction;
+
+import jsfjpa.entity.Automovel;
+import jsfjpa.utils.JPAUtil;
+
+@ManagedBean
+public class AutomovelBean {
+	
+	private Automovel automovel = new Automovel();
+	
+	private List<Automovel> automoveis;
+	
+	public void salvar(Automovel automovel){
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		em.getTransaction().begin();
+		em.persist(automovel);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public void exclui(Automovel automovel){
+		EntityManager em = JPAUtil.getEntityManager();
+		em.getTransaction().begin();
+		automovel = em.merge(automovel);
+		em.remove(automovel);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public List<Automovel> getAutomoveis(){
+		
+		if(this.automoveis == null){
+			EntityManager em = JPAUtil.getEntityManager();
+			Query query = em.createQuery("from Automovel", Automovel.class);
+			this.automoveis = query.getResultList();
+			em.close();
+		}
+				
+		return this.automoveis;
+	}
+	
+
+	public Automovel getAutomovel() {
+		return automovel;
+	}
+	
+	
+
+	
+
+}
+
